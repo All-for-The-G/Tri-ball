bab04356c5bdbd7971df9572921d39ff6fd00326
Em3rgencyLT <em3rgency.lt@gmail.com>
2019-06-16 19:27:39 +0300

    The end of Unity Collaborate garbage

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private int width = 6;
+    [SerializeField] private int height = 6;
+    [SerializeField] private Color defaultColor = Color.white;
+
+    [SerializeField] private Hexagon hexagonPrefab;
+    
+    private List<Hexagon> cells;
+    private HexagonMesh hexagonMesh;
+
+    private void Awake () 
+    {
+        cells = new List<Hexagon>();
+        hexagonMesh = GetComponentInChildren<HexagonMesh>();
+
+        for (int z = 0; z < height; z++) {
+            for (int x = 0; x < width; x++) {
+                CreateCell(x, z);
+            }
+        }
+    }
+    
+    private void Start () 
+    {
+        hexagonMesh.Triangulate(cells);
+    }
+
+    public void ColorCell (Vector3 position, Color color) {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = Hexagon.Index(coordinates, width);
+        Hexagon cell = cells[index];
+        cell.Color = color;
+        hexagonMesh.Triangulate(cells);
+    }
+	
+    private void CreateCell (int x, int z) 
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (GridSettings.INNER_RADIUS * 2f);
+        position.y = 0f;
+        position.z = z * (GridSettings.OUTER_RADIUS * 1.5f);
+
+        Hexagon cell = Instantiate(hexagonPrefab);
+        cells.Add(cell);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.Coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.Color = defaultColor;
+        
+        //FIXME: this logic hurts my soul. Should be a more elegant way to write this.
+        int index = Hexagon.Index(cell.Coordinates, width);
+        if (x > 0) {
+            cell.SetNeighbor(HexagonDirection.W, cells[index - 1]);
+            if (z > 0 && (z & 1) == 0) //(z & 1) == 0 checks if its even
+            {
+                cell.SetNeighbor(HexagonDirection.SW, cells[index - width - 1]);
+            }
+        }
+        if (z > 0) {
+            if ((z & 1) == 0) {
+                cell.SetNeighbor(HexagonDirection.SE, cells[index - width]);
+            }
+            else {
+                cell.SetNeighbor(HexagonDirection.SW, cells[index - width]);
+                if (x < width - 1) {
+                    cell.SetNeighbor(HexagonDirection.SE, cells[index - width + 1]);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    [SerializeField] private HexCoordinates coordinates;
+    [SerializeField] private Color color;
+    [SerializeField] private List<Hexagon> neighbors; //Not not initialize in code. Set size in prefab instead.
+
+    public HexCoordinates Coordinates
+    {
+        get => coordinates;
+        set => coordinates = value;
+    }
+
+    public Color Color
+    {
+        get => color;
+        set => color = value;
+    }
+    
+    public Hexagon GetNeighbor (HexagonDirection direction) {
+        return neighbors[(int)direction];
+    }
+
+    public static int Index(HexCoordinates coordinates, int width)
+    {
+        return coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+    }
+    
+    public void SetNeighbor (HexagonDirection direction, Hexagon cell) {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
+}
