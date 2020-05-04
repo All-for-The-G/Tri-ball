6136b3b8a9dbdba3bbf7749d785b7f8964ae9ba1
Almahmudrony <almahmudrony@gmail.com>
2020-05-04 15:44:24 +0600

    fear(hexagon): Hexagon Props texted.

--- Assets/MapEditorPlugin/Scripts/MapEditor.cs
+++ Assets/MapEditorPlugin/Scripts/MapEditor.cs
@@ -21,20 +21,38 @@ namespace TriBallMapEditorPlugin
     [ExecuteInEditMode]
     public class MapEditor : MonoBehaviour
     {
-        [HideInInspector] public float radius = 3.4f;
+        [HideInInspector] public float radius = 1.9f;
         [HideInInspector] public int width = 10;
         [HideInInspector] public int height = 10;
         [HideInInspector] public TileType[,] grid = new TileType[0,0];
         //public Vector3[] hexagonPos = new Vector3[] { };
         [HideInInspector] public string mapEditorScenePath = "Assets/MapEditorPlugin/MapEditorPlugin_demo_scene.unity";
+#if UNITY_EDITOR
+        public Transform mapGarbage;
+        public Transform debugCube;
+        public MapAssets mapAssets;
         public TriBallMenuConfig menuConfig;
+        public bool captureMode = true;
+        public bool mapDebug = false;
+#endif
         private void Start()
         {
             Init();
         }
         private void Init()
         {
-            GenerateMap();
+#if UNITY_EDITOR
+            if (0<mapGarbage.childCount)
+            {
+                Debug.Log("Garbage was filled last time!");
+                List<GameObject> trash = new List<GameObject>();
+                for (int i = 0; i < mapGarbage.childCount; i++)
+                {
+                    trash.Add(mapGarbage.GetChild(mapGarbage.childCount-1).gameObject);
+                }
+                trash.ForEach((t)=> { DestroyImmediate(t); });
+            }
+#endif
             Selection.activeGameObject = gameObject;
         }
         public void GenerateMap()
@@ -72,6 +90,12 @@ namespace TriBallMapEditorPlugin
         public Texture blocks = null;
         public Texture editorLogo = null;
     }
+
+    [Serializable]
+    public class MapAssets
+    {
+        public GameObject HexagonPrefab;
+    }
 #if UNITY_EDITOR
     [CustomEditor(typeof(MapEditor))]
     //[CanEditMultipleObjects]
@@ -83,8 +107,11 @@ namespace TriBallMapEditorPlugin
         Vector3 a;
         bool repaint = false;
         Vector3 selectedPos = Vector2.one * 1000f;
-        //public Color selectionColor = Color.white;
+        public Color selectionColor = Color.white;
         public TileType paintTileType = TileType.DEFAULT;
+        private List<GameObject> allHexagones = new List<GameObject>();
+        private List<GameObject> hexagonePool = new List<GameObject>();
+        private bool mouseDown = false;
         InspectorPlugin()
         {
             instance = this;
@@ -115,11 +142,41 @@ namespace TriBallMapEditorPlugin
         public void OnSceneGUI()
         {
             mapEditor = (MapEditor)target;
+            if (mapEditor==null && mapEditor.debugCube==null) return;
             Event guiEvent = Event.current;
 
             if (guiEvent.type == EventType.Repaint)
             {
-                DrawHexagon(/*selectedPos*/Vector3.zero, Color.red, true);
+                if(mapEditor.grid.Length == 0)
+                {
+                    DrawHexagon(/*selectedPos*/Vector3.zero, Color.green, true);
+                }
+                mapEditor.debugCube.gameObject.SetActive(mouseDown);
+                if (mouseDown)
+                {
+                    DrawHexagon(selectedPos/*Vector3.zero*/, selectionColor, true);
+                    mapEditor.debugCube.transform.position = selectedPos;
+                    Collider[] colliders = Physics.OverlapSphere(selectedPos, mapEditor.radius);
+                    if (0 < colliders.Length)
+                    {
+                        Renderer rr = null;
+                        float minDistance = mapEditor.radius * 2;
+                        for (int i=0;i<colliders.Length;i++)
+                        {
+                            float distance = Vector3.Distance(selectedPos,colliders[i].transform.position);
+                            if (distance<=minDistance)
+                            {
+                                minDistance = distance;
+                                rr = colliders[i]?.GetComponent<Renderer>();
+                            }
+                        }
+                        if (rr!=null)
+                        {
+                            rr.material.color = selectionColor;
+                        }
+                    }
+                }
+                if(mapEditor.mapDebug)
                 DrawMap();
             }
             else if (guiEvent.type == EventType.Layout)
@@ -149,6 +206,11 @@ namespace TriBallMapEditorPlugin
                 //Debug.Log("Mouse Ray: " + rayHitPos);
                 selectedPos = rayHitPos;
                 UpdateSelection(selectedPos);
+                mouseDown = true;
+            }
+            else
+            {
+                mouseDown = false;
             }
 
             if (_guiEvent.type == EventType.MouseUp && _guiEvent.button == 0)
@@ -184,7 +246,7 @@ namespace TriBallMapEditorPlugin
                     }
                     else if (tt == TileType.DIRT)
                     {
-                        tileColor = Color.grey;
+                        tileColor = Color.red;
                     }
                     else if (tt == TileType.WATER)
                     {
@@ -217,7 +279,7 @@ namespace TriBallMapEditorPlugin
                     Vector3 pos = Vector2.zero;
                     pos.x += -(h * r) / 2 + (i * r) + offset; // for hex like structure
                     pos.y += (w * (r - r / 7)) / 2 - (j * (r - r / 7));
-                    if (Vector3.Distance(selectedPos, new Vector3(pos.x,0,pos.y)) < 1.7)
+                    if (Vector3.Distance(selectedPos, new Vector3(pos.x,0,pos.y)) < 1.1)
                     {
                         me.grid[i, j] = paintTileType;
                     }
@@ -241,18 +303,22 @@ namespace TriBallMapEditorPlugin
             if (GUILayout.Button(mapEditor.menuConfig.grass/*"Reset Area"*/, GUILayout.Width(100), GUILayout.Height(100)))
             {
                 paintTileType = TileType.GRASS;
+                selectionColor = Color.green;
             }
             if (GUILayout.Button(mapEditor.menuConfig.dirt/*"Reset Area"*/, GUILayout.Width(100), GUILayout.Height(100)))
             {
-                paintTileType = TileType.DIRT;
+                paintTileType = TileType.DIRT; ;
+                selectionColor = Color.red;
             }
             if (GUILayout.Button(mapEditor.menuConfig.water/*"Reset Area"*/, GUILayout.Width(100), GUILayout.Height(100)))
             {
-                paintTileType = TileType.WATER;
+                paintTileType = TileType.WATER; ;
+                selectionColor = Color.blue;
             }
             if (GUILayout.Button(mapEditor.menuConfig.blocks/*"Reset Area"*/, GUILayout.Width(100), GUILayout.Height(100)))
             {
-                paintTileType = TileType.BLOCKED;
+                paintTileType = TileType.BLOCKED; ;
+                selectionColor = Color.black;
             }
             Rect newRect = new Rect();
             newRect.Set(110, 20, 100, 300);
@@ -276,7 +342,9 @@ namespace TriBallMapEditorPlugin
             GUILayout.EndHorizontal();
             if (GUILayout.Button("Generate Grid"))
             {
+                ClearHexagones();
                 me.GenerateMap();
+                GenerateHexagonGrid();
             }
             if (GUILayout.Button("Load Map"))
             {
@@ -292,16 +360,25 @@ namespace TriBallMapEditorPlugin
         private void DrawHexagon(Vector3 _pos, Color _default, bool _ignoreSelection = false)
         {
             Vector3[] hexagon = new Vector3[] {
-                    new Vector3(0f,0f,1.7f)+_pos,
-                    new Vector3(1.7f,0f,1.2f)+_pos,
-                    new Vector3(1.7f,0f,-1.2f)+_pos,
-                    new Vector3(0f,0f,-1.7f)+_pos,
-                    new Vector3(-1.7f,0f,-1.2f)+_pos,
-                    new Vector3(-1.7f,0f,1.2f)+_pos
+                    new Vector3(0.86f,0f,0.5f)+_pos,
+                    new Vector3(0.86f,0f,-0.5f)+_pos,
+                    new Vector3(0.0f,0f,-1f)+_pos,
+                    new Vector3(-0.86f,0f,-0.5f)+_pos,
+                    new Vector3(-0.86f,0f,0.5f)+_pos,
+                    new Vector3(0.0f,0f,1.0f)+_pos
                 };
+            _default.a = .4f;
             Handles.color = _default;
             Handles.DrawAAConvexPolygon(hexagon);
-            Handles.color = Color.red;
+            Handles.color = Color.red; hexagon = new Vector3[] {
+                    new Vector3(0.86f,0f,0.5f)+_pos,
+                    new Vector3(0.86f,0f,-0.5f)+_pos,
+                    new Vector3(0.0f,0f,-1f)+_pos,
+                    new Vector3(-0.86f,0f,-0.5f)+_pos,
+                    new Vector3(-0.86f,0f,0.5f)+_pos,
+                    new Vector3(0.0f,0f,1.0f)+_pos,
+                    new Vector3(0.86f,0f,0.5f)+_pos
+                };
             Handles.DrawPolyLine(hexagon);
             Handles.color = Color.white;
         }
@@ -351,11 +428,95 @@ namespace TriBallMapEditorPlugin
         {
             //Debug.Log(Selection.activeObject);
             if (Selection.activeObject != null && Selection.activeGameObject == null) return;
+            if (mapEditor == null) return;
+            if(mapEditor.captureMode)
             Selection.activeObject = target;
         }
-        public void FocusPosition(Vector3 pos)
+        private void FocusPosition(Vector3 pos)
         {
             SceneView.lastActiveSceneView.Frame(new Bounds(pos, Vector3.one), false);
+        }
+        private void ClearHexagones()
+        {
+            if ((allHexagones.Count + hexagonePool.Count)<1000)
+            {
+                allHexagones.ForEach((hexa) => { hexagonePool.Add(hexa); hexa.SetActive(false); });
+                allHexagones.Clear();
+            }
+            allHexagones.ForEach((hexa) => { Destroy(hexa); });
+            allHexagones.Clear();
+        }
+        private void CreateNewHexagon(Vector3 _position)
+        {
+            MapEditor me = (MapEditor)target;
+            if (me == null || me.mapAssets == null || me.mapAssets.HexagonPrefab == null || me.mapGarbage == null) return;
+            if (0<hexagonePool.Count)
+            {
+                GameObject hexaPooled = hexagonePool[hexagonePool.Count - 1];
+                hexagonePool.Remove(hexaPooled);
+                hexaPooled.SetActive(true);
+                hexaPooled.transform.SetParent(me.mapGarbage);
+                hexaPooled.transform.localScale = Vector3.one;
+                hexaPooled.transform.position = _position;
+                allHexagones.Add(hexaPooled);
+
+                Renderer rrp = hexaPooled.GetComponent<Renderer>();
+                var tempMaterial = new Material(rrp.sharedMaterial);
+                tempMaterial.color = Color.white;
+                rrp.material = tempMaterial;
+                return;
+            }
+            GameObject newHexa = Instantiate(me.mapAssets.HexagonPrefab);
+            newHexa.transform.SetParent(me.mapGarbage);
+            newHexa.transform.localScale = Vector3.one;
+            newHexa.transform.position = _position;
+            allHexagones.Add(newHexa);
+            Renderer rr = newHexa.GetComponent<Renderer>();
+            var tm = new Material(rr.sharedMaterial);
+            tm.color = Color.white;
+            rr.material = tm;
+        }
+        private void GenerateHexagonGrid()
+        {
+            MapEditor me = (MapEditor)target;
+            if (me == null || (me.width * me.height != me.grid.Length)) return;
+            float w = me.width;
+            float h = me.height;
+            float r = me.radius;
+            for (int i = 0; i < (int)w; i++)
+            {
+                for (int j = 0; j < (int)h; j++)
+                {
+                    Color tileColor = Color.red;
+                    TileType tt = me.grid[i, j];
+                    if (tt == TileType.DEFAULT)
+                    {
+                        tileColor = Color.white;
+                    }
+                    else if (tt == TileType.GRASS)
+                    {
+                        tileColor = Color.green;
+                    }
+                    else if (tt == TileType.DIRT)
+                    {
+                        tileColor = Color.grey;
+                    }
+                    else if (tt == TileType.WATER)
+                    {
+                        tileColor = Color.blue;
+                    }
+                    else if (tt == TileType.BLOCKED)
+                    {
+                        tileColor = Color.black;
+                    }
+                    float offset = j % 2 == 0 ? r / 4 : 3 * r / 4;
+                    Vector3 pos = Vector2.zero;
+                    pos.x += -(h * r) / 2 + (i * r) + offset; // for hex like structure
+                    pos.y += (w * (r - r / 7)) / 2 - (j * (r - r / 7));
+                    //DrawHexagon(new Vector3(pos.x, 0, pos.y), tileColor);
+                    CreateNewHexagon(new Vector3(pos.x, 0, pos.y));
+                }
+            }
         }
     }
     public class WindowPlugin : EditorWindow
